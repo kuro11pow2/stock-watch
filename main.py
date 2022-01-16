4252d5e9798371d16bc5a1fe2b0bc7bd7af96264
kuro11pow2 <kuro11pow2@gmail.com>
2022-01-16 18:35:36 +0900

    실행점 추가 및 클래스 정리

---
+++ main.py
@@ -0,0 +1,20 @@
+# https://opendart.fss.or.kr/guide/detail.do?apiGrpCd=DS003&apiId=2019016
+
+
+from pprint import pprint
+from src.corporation import corporation
+
+
+def main(cert_key):
+
+    corp_code = '00126380'
+    bsns_year = '2020'
+    reprt_code = '11011'
+    # 1분기보고서 : 11013
+    # 반기보고서 : 11012
+    # 3분기보고서 : 11014
+    # 사업보고서 : 11011
+
+    corp = corporation(cert_key, corp_code, bsns_year, reprt_code)
+    info_arr = corp.main_account_info()
+    pprint([*map(lambda x: (x['thstrm_dt'], x['fs_nm'], x['account_nm'], x['thstrm_amount']), info_arr)])
