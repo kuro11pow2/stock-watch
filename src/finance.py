5af1a52e48d4c25d07fc1ee7fbf63d4fc7b6f12b
kuro11pow2 <kuro11pow2@gmail.com>
2022-01-30 17:37:35 +0900

    클래스명 변경

---
+++ src/finance.py
@@ -0,0 +1,46 @@
+
+import json
+from src.request import Request
+
+# https://opendart.fss.or.kr/guide/detail.do?apiGrpCd=DS003&apiId=2019016
+
+class Finance:
+
+    def __init__(self, corp_code, crtfc_key, url=None):
+        self._corp_code = corp_code
+        self._return_type = '.json'
+        self._company_info_raw = None
+        self._main_account_info_raw = None
+
+        self._cert_key = crtfc_key
+        self._url = 'https://opendart.fss.or.kr/api/'
+
+    def company_info_raw(self):
+        if self._company_info_raw is None:
+            print(f'INFO: dart company_info_raw 조회 {self._corp_code=}')
+            target = 'company'
+            params = {'crtfc_key': self._cert_key , 'corp_code': self._corp_code}
+            response = Request(self._url + target + self._return_type, params).response()
+            self._company_info_raw = json.loads(response.text)
+
+        return self._company_info_raw
+    
+    def main_account_info_raw(self, bsns_year, reprt_code):
+        if self._main_account_info_raw is None:
+            print(f'INFO: dart main_account_info_raw 조회 {self._corp_code=}, {bsns_year=}, {reprt_code=}')
+            target = 'fnlttSinglAcnt'
+            params = {'crtfc_key': self._cert_key , 'corp_code': self._corp_code, 'bsns_year': bsns_year, 'reprt_code': reprt_code}
+            response = Request(self._url + target + self._return_type, params).response()
+            self._main_account_info_raw = json.loads(response.text)
+
+            if self._main_account_info_raw['status'] != '000':
+                print('WARN: main_account_info 조회 불가')
+                self._main_account_info_raw['list'] = ''
+
+        return self._main_account_info_raw
+
+    def main_acount_info(self, bsns_year, reprt_code):
+        return self.main_account_info_raw(bsns_year, reprt_code)['list']
+
+    def _clear_cache(self):
+        self.__init__(self._corp_code, self._cert_key)
